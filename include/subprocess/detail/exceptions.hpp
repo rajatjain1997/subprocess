09db98c1d5e7f8ad1aac3541e4b1fd08ba852ccd
Rajat Jain <1997.rajatjain@gmail.com>
2021-09-12 11:25:22 +0530

    Added more docstrings

--- include/subprocess/detail/exceptions.hpp
+++ include/subprocess/detail/exceptions.hpp
@@ -3,6 +3,14 @@
 
 namespace subprocess
 {
+
+/**
+ * @brief A catch-all class for all errors thrown by subprocess.
+ *
+ * All exceptions in the library derive from subprocess_error.
+ * An exception of type subprocess_error is never actually thrown.
+ *
+ */
 class SUBPROCESS_EXPORT subprocess_error : public std::runtime_error
 {
 public:
@@ -10,6 +18,14 @@ public:
   explicit subprocess_error(const char* str) : runtime_error(str) {}
 };
 
+/**
+ * @brief Thrown when there is an error at the operating system level
+ *
+ * Thrown whenever an error code is returned from a syscall and
+ * subprocess is unable to proceed with the function called by
+ * the user.
+ *
+ */
 class SUBPROCESS_EXPORT os_error : public subprocess_error
 {
 public:
@@ -17,12 +33,30 @@ public:
   explicit os_error(const char* str) : subprocess_error(str) {}
 };
 
+/**
+ * @brief Thrown when there is an errror in the usage of subprocess' public interface
+ *
+ * These errors should be infrequent on the user end.
+ * They get thrown whenever an erroneous call is made to a function within the
+ * library.
+ *
+ * For example, usage_error is thrown when you try to link an fd that is already linked
+ * to another fd.
+ *
+ */
 class SUBPROCESS_EXPORT usage_error : public subprocess_error
 {
 public:
   explicit usage_error(const std::string& str) : subprocess_error(str) {}
   explicit usage_error(const char* str) : subprocess_error(str) {}
 };
+
+/**
+ * @brief Thrown when a command exits with a non-zero exit code.
+ *
+ * This exception is thrown whenever a subprocess constructed from subprocess::command
+ * exits with an error. The return_code() member function can be called to get the return code.
+ */
 
 class SUBPROCESS_EXPORT command_error : public subprocess_error
 {
@@ -34,6 +68,11 @@ public:
   }
   explicit command_error(const char* str, int ret_code) : subprocess_error(str), return_code_{ret_code} {}
 
+  /**
+   * @brief Returns the exit code of the subprocess
+   *
+   * @return int exit code
+   */
   int return_code() { return return_code_; }
 };
 } // namespace subprocess
--- include/subprocess/detail/file_descriptor.hpp
+++ include/subprocess/detail/file_descriptor.hpp
@@ -8,10 +8,35 @@
 
 namespace subprocess
 {
+
+/**
+ * @brief Abstracts file descriptors
+ *
+ * Member functions of this class wrap syscalls to commonly
+ * used file descriptor functions.
+ *
+ * The implementation of this class will be changed in the future with #4.
+ *
+ * @see https://github.com/rajatjain1997/subprocess/issues/4
+ *
+ */
 class SUBPROCESS_EXPORT file_descriptor
 {
 public:
+  /**
+   * @brief Opens a file and returns a file_descriptor object
+   *
+   * @param file_name Name of the file to open
+   * @param flags Flags with which to open the file
+   * @return file_descriptor FD representing the opened file
+   */
   static file_descriptor open(std::filesystem::path file_name, int flags);
+
+  /**
+   * @brief Create an OS level pipe and return read and write fds.
+   *
+   * @return std::pair<file_descriptor, file_descriptor> A pair of linked file_descriptos [read_fd, write_fd]
+   */
   static std::pair<file_descriptor, file_descriptor> create_pipe();
 
   file_descriptor(int fd = -1) : fd_{fd} {}
@@ -29,8 +54,28 @@ public:
 
   void close();
   void close_linked();
+
+  /**
+   * @brief Wrapper for dup2 system call
+   *
+   * duplicates the fd to another
+   *
+   * @param other
+   */
   void dup(const file_descriptor& other);
+
+  /**
+   * @brief Writes a given string to fd
+   *
+   * @param input
+   */
   void write(std::string& input);
+
+  /**
+   * @brief Read from fd and return std::string
+   *
+   * @return std::string Contents of fd
+   */
   std::string read();
 
 private:
@@ -45,9 +90,34 @@ private:
   friend void link(file_descriptor& fd1, file_descriptor& fd2);
 };
 
+/**
+ * @brief Returns an abstraction of stdin file descriptor
+ *
+ * @return file_descriptor stdin
+ */
 SUBPROCESS_EXPORT file_descriptor in();
+
+/**
+ * @brief Returns an abstraction of stdout file descriptor
+ *
+ * @return file_descriptor stdout
+ */
 SUBPROCESS_EXPORT file_descriptor out();
+
+/**
+ * @brief Returns an abstraction of stderr file descriptor
+ *
+ * @return file_descriptor stderr
+ */
 SUBPROCESS_EXPORT file_descriptor err();
 
+/**
+ * @brief Links two file descriptors
+ *
+ * This function is used to link a read and write file descriptor
+ *
+ * @param fd1
+ * @param fd2
+ */
 SUBPROCESS_EXPORT void link(file_descriptor& fd1, file_descriptor& fd2);
 } // namespace subprocess
