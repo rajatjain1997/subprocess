a9005d2eb083a06e59e91ec3ee6bf901a4a559c9
Rajat Jain <1997.rajatjain@gmail.com>
2021-09-18 21:29:25 +0530

    Added more test cases

--- test/source/subprocess_test.cpp
+++ test/source/subprocess_test.cpp
@@ -6,27 +6,42 @@
 
 using subprocess::command;
 
-TEST_CASE("testing simple echo command") { CHECK(command{"echo running correctly"}.run() == 0); }
+TEST_CASE("simple echo command") { CHECK(command{"echo running correctly"}.run() == 0); }
 
-TEST_CASE("testing output redirection")
+TEST_CASE("variable output redirection")
 {
   std::string output;
   (command{"echo abc"} > output).run();
-  CHECK(output == "abc\n");
+  CHECK_EQ(output, "abc\n");
 }
 
-TEST_CASE("testing input redirection")
+TEST_CASE("variable input redirection")
 {
   std::string input{"1\n2\n3\n4\n5"}, output;
   (command{"head -n2"}<input> output).run();
-  CHECK(output == "1\n2\n");
+  CHECK_EQ(output, "1\n2\n");
 }
 
-TEST_CASE("testing piping")
+TEST_CASE("piping")
 {
-  std::string output;
   int errc = (command{"ps aux"} | command{"awk '{print $2}'"} | command{"sort"} | command{"uniq"} |
-              command{"head -n1"} > output)
+              command{"head -n1"} > "/dev/null")
                  .run();
-  CHECK(errc == 0);
+  CHECK_EQ(errc, 0);
+}
+
+TEST_CASE("os_error throw on bad command")
+{
+  REQUIRE_THROWS_AS(command{"random-unavailable-cmd"}.run(), subprocess::exceptions::os_error);
+}
+
+TEST_CASE("command_error throw on bad exit status")
+{
+  REQUIRE_THROWS_AS(command{"false"}.run(), subprocess::exceptions::command_error);
+}
+
+TEST_CASE("nothrow variant doesn't throw on bad exit status")
+{
+  REQUIRE_NOTHROW(command{"false"}.run(std::nothrow));
+  CHECK_NE(command{"false"}.run(std::nothrow), 0);
 }
